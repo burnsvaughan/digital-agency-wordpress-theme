29f86b8a6cc172d5328e7b55f611f943a7ed5afc
Burns Vaughan <47879471+burnsvaughan@users.noreply.github.com>
2019-05-11 16:17:32 +1200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,36 +1,31 @@
 <?php  
-
-function load_stylesheets() {
-
-wp_enqueue_style('wp-block-library-css', 'https://www.warschawski.com/wp-includes/css/dist/block-library/style.min.css?ver=5.1.1');
-wp_enqueue_style('wpr-fonts-css', 'https://use.typekit.net/pbi7izw.css');  
-wp_enqueue_style('wpr-site-css', 'https://www.warschawski.com/wp-content/themes/warschawski/public/css/site.css?ver=1.2.9');
-
-}
-
-add_action('wp_enqueue_scripts', 'load_stylesheets');
-
-function addjs() {
-
-    wp_enqueue_script('jquery', 'https://www.warschawski.com/wp-includes/js/jquery/jquery.js');
-    wp_enqueue_script('main', 'https://www.warschawski.com/wp-content/themes/warschawski/public/js/site.js?ver=1.2.9');
-    wp_enqueue_script('wp-embed', 'https://www.warschawski.com/wp-includes/js/wp-embed.min.js?ver=5.1.1');
-
-}
-
-add_action('wp_enqueue_scripts', 'addjs');
-
-add_theme_support('menus');
-
-register_nav_menus(
-
-    array(
-
-        'top-menu' => __('Top Menu', 'theme'),
-
-    )
-
+	function load_stylesheets() {
+	
+	wp_enqueue_style('wp-block-library-css', 'https://www.warschawski.com/wp-includes/css/dist/block-library/style.min.css?ver=5.1.1');
+	wp_enqueue_style('wpr-fonts-css', 'https://use.typekit.net/pbi7izw.css');  
+	wp_enqueue_style('wpr-site-css', 'https://www.warschawski.com/wp-content/themes/warschawski/public/css/site.css?ver=1.2.9');
+	
+	}
+	
+	add_action('wp_enqueue_scripts', 'load_stylesheets');
+	
+	function addjs() {
+	
+	    wp_enqueue_script('jquery', 'https://www.warschawski.com/wp-includes/js/jquery/jquery.js');
+	    wp_enqueue_script('main', 'https://www.warschawski.com/wp-content/themes/warschawski/public/js/site.js?ver=1.2.9');
+	    wp_enqueue_script('wp-embed', 'https://www.warschawski.com/wp-includes/js/wp-embed.min.js?ver=5.1.1');
+	
+	}
+	
+	add_action('wp_enqueue_scripts', 'addjs');
+	
+	add_theme_support('menus');
+	
+	register_nav_menus(
+	
+	    array(
+	
+	        'top-menu' => __('Top Menu', 'theme'),
+	    )
     );
-
 ?>
-
